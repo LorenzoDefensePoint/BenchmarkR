9163d64a1ad753d1ad48c1f4870c8d34e14911cb
Gabriel Lorenzo - Defense Point <defensepoint@Gabriels-MacBook-Air.local>
2025-04-29 15:40:47 -0300

    fix hardcoded password

--- r-sast-demo/R/app.R
+++ r-sast-demo/R/app.R
@@ -5,7 +5,7 @@ library(httr)
 # API configuration
 api_key <- "sk_test_1234567890abcdef3"
 api_key <- "sk_test_1234543534bcdef3"
-password <- "sajifnenciemap"
+
 # User data access function
 check_user_access <- function(user_role) {
     if (user_role == "user") {
